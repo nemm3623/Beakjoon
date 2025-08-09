2e451ddc6428c9430cefc850e2476050e716dfbd
6zeropk <78200180+nemm3623@users.noreply.github.com>
2025-08-10 00:52:41 +0900

    [Bronze I] Title: 소인수분해, Time: 104 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -9,28 +9,19 @@ public class Main {
 
         int N = Integer.parseInt(br.readLine());
 
-        boolean[] isNotPrime = new boolean[N+1];
-        isNotPrime[0] = true;
-        isNotPrime[1] = true;
-
         for (int i = 2; i <= Math.sqrt(N); i++) {
-            if(isNotPrime[i])
-                continue;
-            for (int j = i*i; j <= N; j += i) {
-                isNotPrime[j] = false;
-            }
-        }
-
-        int i = 2;
-
-        while (N > 1) {
-            if(!isNotPrime[i] && N % i == 0){
+            while (N % i == 0) {
                 System.out.println(i);
                 N /= i;
-                i = 2;
-                continue;
             }
-            i++;
         }
+
+        // N == 1 이면 while() 문에서 소인수 분해가 완전 종료
+        // N != 1 이면 while() 문에서
+        // 실행한 소수보다 더 작은 소수가 남아있음을 의미함으로
+        // 나머지 몫을 출력
+        if( N != 1)
+            System.out.println(N);
+
     }
 }
