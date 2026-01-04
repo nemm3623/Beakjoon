2b774c112f39fcfeeebf7a1638ac61c3e46f68e0
6zeropk <78200180+nemm3623@users.noreply.github.com>
2026-01-04 19:50:16 +0900

    [Bronze II] Title: Hashing, Time: 108 ms, Memory: 14208 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -1,22 +1,21 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int L = Integer.parseInt(br.readLine());
+		String S = br.readLine();
+		long result = 0;
+		long pow = 1;
+		for(int i = 0; i < L; i++) {
+			result += ((S.charAt(i) - 96) * pow);
+			//pow는 31을 매번 곱해준다. 곱해줄때마다 1234567891을 나눠주면 long을 넘지 않을 것이다.
+			pow = (pow * 31) % 1234567891;
+		}
+		System.out.println(result % 1234567891);
+	}
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int L = Integer.parseInt(br.readLine());
-
-        String line = br.readLine();
-
-        double result = 0;
-
-        for (int i = 0; i < L; i++) {
-            result += ((line.charAt(i) - 96) * Math.pow(31, i)) % 1234567891;
-        }
-        System.out.println((int)result);
-    }
 }
