7d01f0de25d1ed97c481cd2d7d45da422e2ad8a8
6zeropk <78200180+nemm3623@users.noreply.github.com>
2026-01-14 18:11:04 +0900

    [Gold V] Title: 회의실 배정, Time: 624 ms, Memory: 46768 KB -BaekjoonHub

---
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class Main {
+
+    static int[] arrX = {1,-1,0,0};
+    static int[] arrY = {0,0,1,-1};
+    static int[][] arr;
+    static int[][] res;
+
+    static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+
+        StringTokenizer st;
+
+        List<Meeting> meetings = new ArrayList<>(N);
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            meetings.add(new Meeting(start, end));
+        }
+
+        meetings.sort(Comparator.comparingInt(Meeting::getEnd).thenComparingInt(Meeting::getStart).thenComparingInt(Meeting::getValue));
+
+        System.out.println(func(meetings));
+    }
+
+    static int func(List<Meeting> meetings) {
+
+        List<Meeting> res = new ArrayList<>();
+        int currentStart;
+        int currentEnd = 0;
+
+        for (Meeting m : meetings) {
+            int start = m.getStart();
+
+            if(currentEnd > start) continue;
+
+            currentStart = start;
+            currentEnd = m.getEnd();
+            
+            res.add(new Meeting(currentStart, currentEnd));
+        }
+        return res.size();
+    }
+
+    static class Meeting {
+
+        int start;
+        int end;
+        int value;
+
+        public Meeting(int start, int end) {
+            this.start = start;
+            this.end = end;
+            this.value = end - start;
+        }
+        public int getStart(){ return start; }
+        public int getEnd(){ return end;}
+        public int getValue(){ return value;}
+    }
+
+}
