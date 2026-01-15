d9486e295dc946650703c94a7e0e5a12c76cff8f
6zeropk <78200180+nemm3623@users.noreply.github.com>
2026-01-15 16:22:58 +0900

    [Gold V] Title: 토마토, Time: 600 ms, Memory: 129540 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -6,11 +6,10 @@ import java.util.*;
 
 public class Main {
 
-    static int[] moveX = {0, 1, 0, -1,};
-    static int[] moveY = {1, 0, -1, 0,};
+    static int[] moveX = {0, 1, 0, -1};
+    static int[] moveY = {1, 0, -1, 0};
 
     static boolean[][] visited;
-    static int[][] count;
 
     public static void main(String[] args) throws IOException {
 
@@ -29,54 +28,55 @@ public class Main {
             for (int j = 0; j < M; j++) {
                 matrix[i][j] = Integer.parseInt(st.nextToken());
                 if (matrix[i][j] == 1) {
-                    queue.add(new int[]{i, j});
+                    queue.add(new int[]{i, j, 0});
                 }
             }
         }
 
-        bfs(queue, matrix);
+        int result = bfs(queue, matrix);
 
-
-        int res = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                if(!visited[i][j] && matrix[i][j] == 0 ) {
-                    System.out.println(-1);
-                    return;
-                }
-                if(res < count[i][j])
-                    res = count[i][j];
-            }
-        }
-
-        System.out.println(res);
+        System.out.println(result);
     }
 
-    static void bfs(Queue<int[]> queue, int[][] matrix) {
-         visited = new boolean[matrix.length][matrix[0].length];
-        count = new int[matrix.length][matrix[0].length];
+    static int bfs(Queue<int[]> queue, int[][] matrix) {
+        visited = new boolean[matrix.length][matrix[0].length];
+
+
+        for (int[] pos : queue) {
+            visited[pos[0]][pos[1]] = true;
+        }
+
+        int maxDay = 0;
 
         while (!queue.isEmpty()) {
 
             int[] cur = queue.poll();
-
-            visited[cur[0]][cur[1]] = true;
+            int cx = cur[0], cy = cur[1], day = cur[2];
+            maxDay = Math.max(maxDay, day);
 
             for (int i = 0; i < 4; i++) {
-                int x = cur[0] + moveX[i];
-                int y = cur[1] + moveY[i];
-
-
+                int x = cx + moveX[i];
+                int y = cy + moveY[i];
 
                 if (x < 0 || x >= matrix.length || y < 0 || y >= matrix[0].length)
                     continue;
-                if (matrix[x][y] == -1 || matrix[x][y] == 1 || visited[x][y])
+                if (matrix[x][y] == -1 || visited[x][y])
                     continue;
 
-                queue.add(new int[]{x, y});
+                queue.add(new int[]{x, y, day + 1});
                 visited[x][y] = true;
-                count[x][y] = count[cur[0]][cur[1]] + 1;
             }
         }
+
+        // 방문하지 못한 0이 있는지 체크
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] == 0 && !visited[i][j]) {
+                    return -1;
+                }
+            }
+        }
+
+        return maxDay;
     }
 }
